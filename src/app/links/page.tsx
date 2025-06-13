367ea208e94421cef781a0cbea4df8ec7b520208
rum7 <87144903+rum7@users.noreply.github.com>
2025-06-13 19:37:14 +0200

    Static links page created with table and icon component

---
+++ src/app/links/page.tsx
@@ -0,0 +1,117 @@
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { Icons } from "@/components/ui/icons"
+import Link from "next/link"
+import { cn } from "@/lib/utils"
+
+type Datalinks = {
+    category: string[],
+    name: string,
+    address: string[],
+    website: string,
+    socials: {
+        [key: string]: string
+    }
+}
+
+const datalinks: Datalinks[] = [
+    {
+        category: ["food", "bakery", "breakfast", "deserts", "sandwiches"],
+        name: "Boulangerie Paris & CO",
+        address: [
+            "Paris, 4 rue de la convention 75015 Paris France"
+        ],
+        website: "https://boulangerieparisandco.fr/",
+        socials: {
+            instagram: "https://www.instagram.com/boulangerieparisandco/?hl=fr",
+        },
+    },
+    {
+        category: ["food", "bakery", "breakfast", "deserts"],
+        name: "BO&MIE",
+        address: [
+            "PARIS - 91 Rue de Rivoli, 75001 Paris - Tél : +33 1 42 33 49 84",
+            "BARCELONE - C/ de Provença, 433 08025 Barcelone - Tél : +34 934 84 74 60",
+            "AIX-EN-PROVENCE - 9 Route Départementale Gare TGV d, 13290 Aix-en-Provence",
+            "JEDDAH - Centre commercial La Paz - ARABIE SAOUDITE ",
+            "SEOUL - 63 Sogong-ro, Jung District, Seoul, Corée du Sud",
+        ],
+        website: "https://www.boetmie.com/",
+        socials: {
+            facebook: "https://www.facebook.com/boetmie/",
+            instagram: "https://www.instagram.com/boetmie/",
+            linkedin: "https://www.linkedin.com/company/bo-&-mie/"
+        },
+    },
+]
+
+export default function page() {
+    return (
+
+        <div className="">
+            <Table>
+                <TableCaption className="italic">You should give it a try, really...</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[200px]">Category</TableHead>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Address</TableHead>
+                        <TableHead>Website</TableHead>
+                        <TableHead>Socials</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {datalinks.map((datalink, index) => (
+                        <TableRow key={`${datalink.name}-${index}`}>
+                            <TableCell className="font-medium">
+                                <ul className="flex flex-wrap gap-2">
+                                    {datalink.category.map((cat, index) => (
+                                        <li
+                                            key={index}
+                                            className={cn("flex size-5 w-fit p-1 items-center justify-center rounded text-xs font-medium bg-indigo-400/20 text-indigo-500", )}
+                                        >
+                                            {cat}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </TableCell>
+                            <TableCell>{datalink.name}</TableCell>
+                            <TableCell>
+                                <ul className="flex flex-col gap-2">
+                                    {datalink.address.map((address, index) => (
+                                        <li key={index}> {address}</li>
+                                    ))}
+                                </ul>
+                            </TableCell>
+                            <TableCell>
+                                <Link href={datalink.website} target="_blank">{datalink.website}</Link>
+                            </TableCell>
+                            <TableCell>
+                                <div className="flex gap-2">
+                                    {Object.entries(datalink.socials).map(([key, url]) => {                                        
+                                        const IconComponent = Icons[key as keyof typeof Icons]
+                                        if (IconComponent) return <Link key={key} href={url} target="_blank"><IconComponent className="size-6" /></Link>
+                                    })}
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+                {/* <TableFooter>
+                    <TableRow className="italic">
+                        <TableCell>Total</TableCell>
+                        <TableCell colSpan={4} className="">{datalinks.length} things you've to try</TableCell>
+                    </TableRow>
+                </TableFooter> */}
+            </Table>
+        </div>
+    )
+}
